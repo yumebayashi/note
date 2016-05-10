9c0248f7b7bdd4aa4b15c865ea196a9e8c4ae072
yumebayashi <yasutaka.umebayashi@gmail.com>
2016-05-10 14:30:57 +0900

    rm notes dir

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,17 +21,19 @@ AUTHOR_FEED_RSS = None
 
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+#THEME
+THEME = './pelican-blueidea-master'
